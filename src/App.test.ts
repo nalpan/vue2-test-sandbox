a5a0d7d72b9a2ac3a90ed145faf1832f2194a6b5
kazuhiro.nakata <kazuhiro.nakata@rakus.co.jp>
2021-12-13 13:32:22 +0900

    styleの確認を追加

--- src/App.test.ts
+++ src/App.test.ts
@@ -2,27 +2,36 @@ import App from './App.vue'
 import {render, fireEvent} from '@testing-library/vue'
 
 describe('length validate', () => {
-  it('init -> text length ng', async () => {
+  it('init -> "text length ng" and "color: red"', async () => {
     const component = render(App)
+
     const text = component.baseElement.querySelector('.text-length-result span')
     expect(text!.textContent).toBe(('text length ng'))
+    const style = window.getComputedStyle(text!)
+    expect(style.color).toBe('red')
   })
-  it('input 1234567890 -> text length ng', async () => {
+  it('input 1234567890 -> "text length ng" and "color: red"', async () => {
     const component = render(App)
     const input = component.baseElement.querySelector('input')
     await fireEvent.update(input!, "123456789")
     const button = component.baseElement.querySelector('button.button')
     await fireEvent.click(button!)
+
     const text = component.baseElement.querySelector('.text-length-result span')
     expect(text!.textContent).toBe(('text length ng'))
+    const style = window.getComputedStyle(text!)
+    expect(style.color).toBe('red')
   })
-  it('input 1234567890 -> text length ok', async () => {
+  it('input 1234567890 -> "text length ok!!!" and "color: "', async () => {
     const component = render(App)
     const input = component.baseElement.querySelector('input')
     await fireEvent.update(input!, "1234567890")
     const button = component.baseElement.querySelector('button.button')
     await fireEvent.click(button!)
+
     const text = component.baseElement.querySelector('.text-length-result span')
     expect(text!.textContent).toBe(('text length ok!!!'))
+    const style = window.getComputedStyle(text!)
+    expect(style.color).toBe('')
   })
 })
